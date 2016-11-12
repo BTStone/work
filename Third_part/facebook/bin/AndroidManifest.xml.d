6712e8cd460bae32de24a8af0a934acc6d0c723b
石征南 <1160330830@qq.com>
2016-11-12 13:53:17 +0800

    备份

---
+++ Third_part/facebook/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\facebook\bin\AndroidManifest.xml : \
+D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\facebook\AndroidManifest.xml \
---
+++ Third_part/google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\google-play-services_lib\bin\AndroidManifest.xml : \
+D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\google-play-services_lib\AndroidManifest.xml \
