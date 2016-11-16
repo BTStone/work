ab2972f5e89538bfef4c682a5ac65e1277f078c9
石征南 <1160330830@qq.com>
2016-11-16 14:08:22 +0800

    第三方已经新建git库， 从这个库里删除

--- Third_part/facebook/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\facebook\bin\AndroidManifest.xml : \
-D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\facebook\AndroidManifest.xml \
--- Third_part/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\google-play-services_lib\bin\AndroidManifest.xml : \
-D:\installpakage\sdk\cocos2d-x-3.7.1\cocos2d-x-3.7.1\projects\google-play-services_lib\AndroidManifest.xml \
